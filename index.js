88be1bf4cfdbccd0b0436ae8f90f90f18e7b0fe7
Leohuguenin <leonardohuguenin@live.com>
2025-02-24 16:43:44 +1100

    2 looping

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solutions in this file
+const names = ["Leo", "Vanessa", "Zoe"]
+const event = "Code Expo"
+
+function writeCards(names, event) {
+    let messages = [];
+
+    for (let i = 0; i < names.length;  i++) {
+        let message = `Thank you, ${names[i]}, for your presence at the ${event}`;
+        messages.push(message);
+    }
+
+    return messages;
+}
+const thankYouCards = writeCards(names, event);
+console.log(thankYouCards);
+
+function countDown(number) {
+    
+    while (number >= 0) {
+      console.log(number); 
+      number--; 
+    }
+  }
